678660aa12acd2a382379d27719759e0eadcbfe6
DenBuzz <trey@wagerfamily.com>
2018-03-28 13:32:14 -0400

    automatic x and y limits

--- curves.py
+++ curves.py
@@ -6,19 +6,16 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-# y = [sigmoid(n) for n in x]
-# plt.plot(x, y)
-# plt.show()
-
-
 def sigmoid(x):
     return 1 / (1 + math.exp(-x))
 
 
 def animate_lines(x, f, g, frames, interval):
 
-    sig_points = np.linspace(-5, 5, frames)
-
+    sig_points = np.linspace(-5, 10, frames)
+    y_min = np.min([np.min(f(x)), np.min(g(x))])
+    y_max = np.max([np.max(f(x)), np.max(g(x))])
+    shift = (y_max-y_min)/10
     def update_lines(i):
         if i == 0:
             time.sleep(1)
@@ -33,19 +30,19 @@ def animate_lines(x, f, g, frames, interval):
     plt.plot(x, g(x), '--')
     line, = plt.plot(x, f(x))
     plt.xlim(x[0], x[-1])
-    plt.ylim(-1.2, 1.2)
+    plt.ylim(y_min-shift, y_max+shift)
     line_ani = animation.FuncAnimation(fig, update_lines, frames, interval=interval, blit=True)
 
     plt.show()
 
 
-x = np.arange(-4 * np.pi, 4 * np.pi, 0.1)
+x = np.arange(-2 * np.pi, 2 * np.pi, 0.1)
 
 
-def f(x): return np.sin(x)**2
+def f(x): return -np.cos(x)**2
 
 
-def g(x): return np.cos(x)
+def g(x): return 1 + np.cos(x)
 
 
 animate_lines(x, f, g, 1000, 3)
