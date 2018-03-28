5caaad361bbc1d8211cbaf949a6b0e7d59bf45ab
DenBuzz <trey@wagerfamily.com>
2018-03-28 14:04:11 -0400

    baeutified

--- curves.py
+++ curves.py
@@ -15,7 +15,8 @@ def animate_lines(x, f, g, frames, interval):
     sig_points = np.linspace(-5, 10, frames)
     y_min = np.min([np.min(f(x)), np.min(g(x))])
     y_max = np.max([np.max(f(x)), np.max(g(x))])
-    shift = (y_max-y_min)/10
+    shift = (y_max - y_min) / 10
+
     def update_lines(i):
         if i == 0:
             time.sleep(1)
@@ -30,7 +31,7 @@ def animate_lines(x, f, g, frames, interval):
     plt.plot(x, g(x), '--')
     line, = plt.plot(x, f(x))
     plt.xlim(x[0], x[-1])
-    plt.ylim(y_min-shift, y_max+shift)
+    plt.ylim(y_min - shift, y_max + shift)
     line_ani = animation.FuncAnimation(fig, update_lines, frames, interval=interval, blit=True)
 
     plt.show()
